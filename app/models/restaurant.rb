7b141d65aeb885596b7af02011f917b73dd61289
Marko Manojlovic <marko@thecookie.org>
2016-05-21 15:03:58 -0700

    modified friendly_id

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ActiveRecord::Base
 	extend FriendlyId
-  	friendly_id :name, use: [:slugged, :finders]
+  	friendly_id :name, use: [:slugged, :finders] # had to add use: like this, instead of just :slugged as it wasn't working from some strange reason
 
 	geocoded_by :address   # can also be an IP address
 	after_validation :geocode          # auto-fetch coordinates
