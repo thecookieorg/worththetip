94f1e9e9f80199722057edefa651e4b129401620
Marko Manojlovic <marko@thecookie.org>
2016-05-21 16:00:28 -0700

    modified users/edit view, added custom route after user login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
 
+  scope :users do
+    root :to => 'restaurants#index', :as => :user_root
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
